44b46c29aa0f9771011b12cbbddc462769f47c1c
satishss075 <satishss075@gmail.com>
2022-07-25 12:53:23 +0530

    hiii

---
+++ src/test/java/monday/Pratice1.java
@@ -0,0 +1,13 @@
+package monday;
+
+public class Pratice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hiii");
+System.out.println("bye");
+System.out.println(" cccc");
+System.out.println("hahahahah");
+	}
+
+}
